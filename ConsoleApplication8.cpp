89aae45bcc5ae97008ddca6d1f49fb2ada03af5d
ylvaperic <ilva.peric4@gmail.com>
2026-01-11 17:46:12 +0100

    Izmjene

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -1,135 +1,102 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-typedef struct Node {
-    double data;
-    struct Node* next;
-} Node;
+struct stog {
+    int el;
+    struct stog* next;
+};
 
-// Push: vraća novi top stoga
-Node* push(Node* top, double value) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (!newNode) {
-        printf("Greska pri alokaciji memorije!\n");
-        return top; // ne uspe, vraća stari top
-    }
-    newNode->data = value;
-    newNode->next = top;
-    return newNode;
+int Push(struct stog* s, int x) {
+    struct stog* q = (struct stog*)malloc(sizeof(struct stog));
+    if (!q) return -1;
+    q->el = x;
+    q->next = s->next;
+    s->next = q;
+    return 1;
 }
 
-// Pop: vraća novi top stoga i čuva vrednost, 0 = neuspeh
-int pop(Node* top, double* value) {
-    if (top == NULL) return 0;
-    Node* temp = top;
-    *value = top->data;
+int Pop(struct stog* s) {
+    struct stog* temp = s->next;
+    if (!temp) return -1;
+    int n = temp->el;
+    s->next = temp->next;
     free(temp);
-    return 1;
+    return n;
 }
 
-// Funkcija koja evaluira postfiks izraz sa razmacima
-int evaluatePostfix(char* expr, double* result) {
-    Node* stack = NULL;
-    int i = 0;
+int Operacija(int op1, int op2, char operand) {
+    switch (operand) {
+    case '+': return op2 + op1;
+    case '-': return op2 - op1;
+    case '*': return op2 * op1;
+    case '/': return op2 / op1;
+    default: return 0;
+    }
+}
 
-    while (expr[i] != '\0') {
-        if (isspace(expr[i])) {
-            i++;
-            continue;
+int UcitajPostfix(char* imedatoteke, char postfix[][100]) {
+    FILE* f = fopen(imedatoteke, "r");
+    if (!f) return -1;
+    int n = 0;
+    while (n < 100 && fgets(postfix[n], 100, f)) {
+        postfix[n][strcspn(postfix[n], "\n")] = '\0';
+        n++;
+    }
+    fclose(f);
+    return n;
+}
+
+void IzracunajPostfix(struct stog s, char postfix[][100], int brojRedaka) {
+    s.next = NULL;
+    int op1, op2, rezultat = 0;
+
+    for (int i = 0; i < brojRedaka; i++) {
+        char* elem = postfix[i];
+        if (isdigit(elem[0])) {
+            int broj = atoi(elem);
+            Push(&s, broj);
         }
-
-        if (isdigit(expr[i]) || (expr[i] == '-' && isdigit(expr[i + 1]))) {
-            // Čitanje broja (pozitivnog ili negativnog)
-            double num = 0;
-            int sign = 1;
-            if (expr[i] == '-') {
-                sign = -1;
-                i++;
+        else if (elem[0] == '+' || elem[0] == '-' || elem[0] == '*' || elem[0] == '/') {
+            op1 = Pop(&s);
+            op2 = Pop(&s);
+            if (op1 == -1 || op2 == -1) {
+                printf("\nGreska: stog je prazan!\n");
+                return;
             }
-            while (isdigit(expr[i])) {
-                num = num * 10 + (expr[i] - '0');
-                i++;
-            }
-            num *= sign;
-            stack = push(stack, num);
-        }
-        else {
-            // Operator
-            double b, a;
-            // Pop b
-            if (stack == NULL) {
-                printf("Greska: nedovoljno operanada za operator '%c'\n", expr[i]);
-                return 0;
-            }
-            b = stack->data;
-            Node* temp = stack;
-            stack = stack->next;
-            free(temp);
-
-            // Pop a
-            if (stack == NULL) {
-                printf("Greska: nedovoljno operanada za operator '%c'\n", expr[i]);
-                return 0;
-            }
-            a = stack->data;
-            temp = stack;
-            stack = stack->next;
-            free(temp);
-
-            switch (expr[i]) {
-            case '+': stack = push(stack, a + b); break;
-            case '-': stack = push(stack, a - b); break;
-            case '*': stack = push(stack, a * b); break;
-            case '/':
-                if (b == 0) {
-                    printf("Greska: deljenje nulom\n");
-                    return 0;
-                }
-                stack = push(stack, a / b);
-                break;
-            default:
-                printf("Nepoznat operator: '%c'\n", expr[i]);
-                return 0;
-            }
-            i++;
+            rezultat = Operacija(op1, op2, elem[0]);
+            Push(&s, rezultat);
         }
     }
 
-    if (stack == NULL) {
-        printf("Greska: stog je prazan, nema rezultata\n");
-        return 0;
-    }
-
-    *result = stack->data;
-    free(stack);
-
-    return 1;
+    printf("%d\n", rezultat);
 }
 
 int main() {
-    FILE* file = fopen("postfix.txt", "r");
-    if (!file) {
-        printf("Greska pri otvaranju datoteke!\n");
+    char imedatoteke[100];
+    char postfix[100][100];
+    int brojRedaka;
+    struct stog s;
+    s.next = NULL;
+
+    printf("Unesi ime datoteke:\n");
+    scanf("%99s", imedatoteke);
+
+    brojRedaka = UcitajPostfix(imedatoteke, postfix);
+    if (brojRedaka == -1) {
+        printf("Greska pri otvaranju datoteke.\n");
         return 1;
     }
 
-    char line[256];
-    if (fgets(line, sizeof(line), file) == NULL) {
-        printf("Datoteka je prazna!\n");
-        fclose(file);
-        return 1;
+    printf("\nUcitani postfix izrazi:\n");
+    for (int i = 0; i < brojRedaka; i++) {
+        printf("%s\n", postfix[i]);
     }
-    fclose(file);
 
-    double result;
-    if (evaluatePostfix(line, &result)) {
-        printf("Rezultat postfiks izraza: %.2lf\n", result);
-    }
-    else {
-        printf("Evaluacija izraza nije uspela.\n");
-    }
+    printf("\nRezultat:\n");
+    IzracunajPostfix(s, postfix, brojRedaka);
 
     return 0;
 }
