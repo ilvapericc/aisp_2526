f4c2327b1d314d3ccfd5cb4a5b5af8079a94b57a
Ilva Perić <153132759+ilvapericc@users.noreply.github.com>
2026-01-12 19:18:54 +0100

    Refactor postfix evaluation and error handling

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -1,8 +1,11 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <limits.h>
+
+#define MAX INT_MIN   // GLOBALNA VARIJABLA ZA SIGNAL GREŠKE
 
 struct stog {
     int el;
@@ -11,7 +14,7 @@ struct stog {
 
 int Push(struct stog* s, int x) {
     struct stog* q = (struct stog*)malloc(sizeof(struct stog));
-    if (!q) return -1;
+    if (!q) return 0;
     q->el = x;
     q->next = s->next;
     s->next = q;
@@ -20,11 +23,17 @@ int Push(struct stog* s, int x) {
 
 int Pop(struct stog* s) {
     struct stog* temp = s->next;
-    if (!temp) return -1;
+    if (!temp) return MAX;   // greška
+
     int n = temp->el;
     s->next = temp->next;
     free(temp);
     return n;
+}
+
+void BrisiSve(struct stog* s) {
+    while (s->next != NULL)
+        Pop(s);
 }
 
 int Operacija(int op1, int op2, char operand) {
@@ -32,14 +41,21 @@ int Operacija(int op1, int op2, char operand) {
     case '+': return op2 + op1;
     case '-': return op2 - op1;
     case '*': return op2 * op1;
-    case '/': return op2 / op1;
-    default: return 0;
+    case '/':
+        if (op1 == 0) {
+            printf("Greska: dijeljenje s nulom!\n");
+            return MAX;
+        }
+        return op2 / op1;
+    default:
+        return MAX;
     }
 }
 
 int UcitajPostfix(char* imedatoteke, char postfix[][100]) {
     FILE* f = fopen(imedatoteke, "r");
     if (!f) return -1;
+
     int n = 0;
     while (n < 100 && fgets(postfix[n], 100, f)) {
         postfix[n][strcspn(postfix[n], "\n")] = '\0';
@@ -55,29 +71,47 @@ void IzracunajPostfix(struct stog s, char postfix[][100], int brojRedaka) {
 
     for (int i = 0; i < brojRedaka; i++) {
         char* elem = postfix[i];
-        if (isdigit(elem[0])) {
+
+        /* PREPOZNAJE I NEGATIVNE BROJEVE */
+        if (isdigit(elem[0]) || (elem[0] == '-' && isdigit(elem[1]))) {
             int broj = atoi(elem);
             Push(&s, broj);
         }
-        else if (elem[0] == '+' || elem[0] == '-' || elem[0] == '*' || elem[0] == '/') {
+        else if (strchr("+-*/", elem[0]) && elem[1] == '\0') {
             op1 = Pop(&s);
             op2 = Pop(&s);
-            if (op1 == -1 || op2 == -1) {
-                printf("\nGreska: stog je prazan!\n");
+
+            if (op1 == MAX || op2 == MAX) {
+                printf("Greska: stog je prazan!\n");
+                BrisiSve(&s);
                 return;
             }
+
             rezultat = Operacija(op1, op2, elem[0]);
+
+            if (rezultat == MAX) {
+                BrisiSve(&s);
+                return;
+            }
+
             Push(&s, rezultat);
+        }
+        else {
+            printf("Nepoznat element: %s\n", elem);
+            BrisiSve(&s);
+            return;
         }
     }
 
-    printf("%d\n", rezultat);
+    printf("Rezultat: %d\n", rezultat);
+    BrisiSve(&s);
 }
 
 int main() {
     char imedatoteke[100];
     char postfix[100][100];
     int brojRedaka;
+
     struct stog s;
     s.next = NULL;
 
@@ -90,12 +124,11 @@ int main() {
         return 1;
     }
 
-    printf("\nUcitani postfix izrazi:\n");
-    for (int i = 0; i < brojRedaka; i++) {
+    printf("\nUcitani postfix izraz:\n");
+    for (int i = 0; i < brojRedaka; i++)
         printf("%s\n", postfix[i]);
-    }
 
-    printf("\nRezultat:\n");
+    printf("\n");
     IzracunajPostfix(s, postfix, brojRedaka);
 
     return 0;
